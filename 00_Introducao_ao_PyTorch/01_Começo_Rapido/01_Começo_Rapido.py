4f683e2eb6a2d428de9ae12ae37c99471b521bf1
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-05-12 13:36:06 -0300

    Update 01

---
+++ 00_Introducao_ao_PyTorch/01_Começo_Rapido/01_Começo_Rapido.py
@@ -0,0 +1,5 @@
+
+
+########## COMEÇO RÁPIDO ##########
+
+    # Esta seção é executada por meio da API para tarefas comuns em aprendizado de máquina. Consulte os links em cada seção para se aprofundar.
