c58bc38caf7ba0904f435385505bdb328644a118
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-05-12 13:35:46 -0300

    Update 01

---
+++ 00_Introducao_ao_PyTorch/00_Aprenda_o_Basico/01_Aprenda_o_Basico.py
@@ -0,0 +1,12 @@
+##### Tutorial retirado de: https://pytorch.org/tutorials/beginner/basics/intro.html
+
+
+########## APRENDA O BÁSICO ##########
+
+    # Autores: Suraj Subramanian, Seth Juarez, Cassie Breviu, Dmitry Soshnikov, Ari Bornstein
+
+    # A maioria dos fluxos de trabalho de aprendizado de máquina envolve trabalhar com dados, criar modelos, otimizar parâmetros de modelo e salvar os modelos treinados. Este tutorial apresenta um fluxo de trabalho de ML completo implementado no PyTorch, com links para saber mais sobre cada um desses conceitos.
+
+    # Usaremos o conjunto de dados FashionMNIST para treinar uma rede neural que prevê se uma imagem de entrada pertence a uma das seguintes classes: camiseta/top, calça, pulôver, vestido, casaco, sandália, camisa, tênis, bolsa ou tornozelo Bota.
+
+    # Este tutorial pressupõe uma familiaridade básica com os conceitos de Python e Deep Learning. 
