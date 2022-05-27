0ccfb46704158a4e901d1fd95cc333b47b3491b3
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-05-26 21:38:08 -0300

    Update 01

---
+++ 00_Introducao_ao_PyTorch/08_Salvar,_Carregar_e_Usar_o_Modelo/01_Salvar_e_Carregar_o_Modelo.py
@@ -0,0 +1,7 @@
+########## SALVAR E CARREGAR O MODELO ##########
+
+    # Nesta seção, veremos como persistir o estado do modelo salvando, carregando e executando as previsões do modelo.
+
+
+import torch
+import torchvision.models as models
